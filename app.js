0306f4911a87f048cc9536e982910a74860aeb0b
dong wang <don_jc123@hotmail.com>
2014-08-15 22:12:45 -0400

    setup dbase

--- app.js
+++ app.js
@@ -13,15 +13,17 @@ var app = express();
 var DB = require('./accessDB');
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.configure(function() {
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
 
 // development only
 if ('development' == app.get('env')) {
